3df4c2a2b86b45a590b636761005c7ce1394c224
ilias <iliasdavid@live.fr>
2021-01-19 20:39:58 +0100

    work on API request and data showing

--- api/api.js
+++ api/api.js
@@ -1,21 +1,34 @@
 export function getApiConnection() {
+		
+	const imageUrl = "http://jouetenbois.mathieu-boucher.fr/api/images/products/";
+	const key = "ws_key=3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE";
+    let url = "http://jouetenbois.mathieu-boucher.fr/api/products/?" + key + "&output_format=JSON&display=[id,name,price,id_default_image]";
+	let infos = "[";				
 
-    let apiKey = '3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE';
+    let listOfProducts = fetch(url)
+        .then( function(response) {
+			if(response.ok) {
+				console.log("ok");
+			} else
+				console.log("ko");
+			return response.json();
+		})
+		.then( function(data) {
+			data.products.forEach(function (product) {
+				infos += "{ "
+							+ "\"name\": \"" + product.name.shift().value + "\", "
+							+ "\"price\": \"" + product.price + "\", "
+							+ "\"image\": \"" + imageUrl + product.id + "/" + product.id_default_image + "?" + key + "\" "
+						+ "},";		
+			});
+			infos = infos.substring(0, infos.length - 1) + "]";
+			return infos;
+		});
+	
+	console.log(infos);
 
-    /*let requestOptions = {
-        method: 'GET',
-        withCredentials: true,
-        mode: 'no-cors',
-        headers: {
-            "Cookie": "PrestaShop-9e5f2b711488cc1814878857d8049247=def50200ad17e1919ecc34879302b0642d1d5394fd78df6278b4168a7568e93d483ee634e5caf533928f8b84ead852084583c88fc06aa9eb3af644bd89ef533cb45cb5d950e55b0416fbd63b3509cecb0754b6bb2f1b7a8c03878f76ce450b36c876b3ede02f7ab6a94329cbb677b47ec5cdc4d34ef809c5bfdb1228bbbd59c082dd05615a740e631c3ca96534e2bbdf6b3670610a0df99deb399280048a4871c3de99bc79a0a4b7c2fee200ffe0fdfe92f645ff564234bfb06161c02c2074e3592a07c5341e140ec57868a2fd1e6de2c3daf2c08a99f0a67e",
-            "Output-Format": "JSON"
-        },
-        redirect: 'follow'
-    };*/
-
-    fetch(`https://jouetenbois.mathieu-boucher.fr/api/?ws_key=${apiKey}&output_format=JSON`)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
+	return JSON.parse(infos);
+ 
 }
+
+export default getApiConnection;
--- components/Products.js
+++ components/Products.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { getApiConnection } from "../api/api";
 
 const styles = StyleSheet.create({
   container: {
@@ -13,56 +14,13 @@ const styles = StyleSheet.create({
   },
 });
 
-async function getListOfPopularProducts() {
-
-  const apiUrl = "http://jouetenbois.mathieu-boucher.fr/api/products?display=[id,description,description_short,name,price,id_default_image]";
-
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Basic M01KRkk2V1lENkdXREdKOFFFQ002VFpBWjZWNjVWVUU=");
-
-  var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };
-
-  fetch(apiUrl, myInit)
-  .then( function(response) {
-    if(response.ok) {
-        console.log("ok");
-    } else
-        console.log("ko");
-    return response.json();
-  })
-  .then( function(json) {
-    
-    json.getElementById("products").forEach(function (product) {
-      console.log("test");
-    })
-
-    let countElement = document.getElementById("taskCount");
-    countElement.innerHTML = "" + countContent();
-
-  });
-}
-
 const Products = () => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => <Text>{item.key}</Text>}
-      />
+        data={getApiConnection()}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+      /> 
     </View>
   );
 }
