dde6e78ac16ad8d6dd821307907478b95afb977e
ilias <iliasdavid@live.fr>
2021-01-20 20:16:53 +0100

    Travail listing

--- components/Products.js
+++ components/Products.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -13,52 +13,56 @@ const styles = StyleSheet.create({
   },
 });
 
-const getApiConnection = async () => {
-		
-  const imageUrl = "http://jouetenbois.mathieu-boucher.fr/api/images/products/";
-  const key = "ws_key=3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE";
-  let url = "http://jouetenbois.mathieu-boucher.fr/api/products/?" + key + "&output_format=JSON&display=[id,name,price,id_default_image]";
-  var infos = "[";				
+const datas = [ 
+  {
+    id: 5, 
+    name: "test",
+  }, 
+  {
+    id: 6, 
+    name: "tefst",
+  },
+];
 
-  await fetch(url)
-  .then( function(response) {
-    if(response.ok) console.log("ok");
-    else console.log("ko");
-    return response.json();
-  })
-  .then( function(data) {
-
-    data.products.forEach(function (product) {
-      infos += " { "
-            + "\"name\": \"" + product.name.shift().value + "\", "
-            + "\"price\": \"" + product.price + "\", "
-            + "\"image\": \"" + imageUrl + product.id + "/" + product.id_default_image + "?" + key + "\" "
-          + "},";		
-    });
-    infos = infos.substring(0, infos.length - 1) + " ]";
-
-  });
-
-  console.log(infos + "\"");
-  var obj = JSON.parse(infos);
-
-  console.log("Product \"" + obj[0].name + "\" :\n"
-			+	"\t- Price : " + obj[0].price + "\n"
-      +	"\t- Image : " + obj[0].image + "\n");
+const getData = () => {
+    const imageUrl = "http://jouetenbois.mathieu-boucher.fr/api/images/products/";
+    const key = "ws_key=3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE";
+    let url = "http://jouetenbois.mathieu-boucher.fr/api/products/?" + key + "&output_format=JSON&display=[id,name,price,id_default_image]";
+    var infos = "{ \"products\": [";
+  
+    return fetch(url)
+    .then( function(response) {
+      if (response.ok) console.log("ok");
+      else console.log("ko");
+      return response.json();
+    })
+    .then( function(data) {
+                  
+      data.products.forEach(function (product) {
+        infos = infos + " { "
+              + "\"name\": \"" + product.name.shift().value + "\", "
+              + "\"price\": \"" + product.price + "\", "
+              + "\"image\": \"" + imageUrl + product.id + "/" + product.id_default_image + "?" + key + "\" "
+            + "},";		
+      });
+      infos = infos.substring(0, infos.length - 1) + " ] }";
       
-  return obj; 
- 
+      console.log(infos);
+            
+      return JSON.parse(infos);
+  });
 }
 
-const Products = () => {
+const Products = () =>
+{
   return (
     <View style={styles.container}>
       <FlatList
-        data={getApiConnection()}  
-        renderItem={{(product)} => <Text>{product.name}</Text>}
+        data={getData()}
+        renderItem={({item}) => {<Text>{item.name}</Text>}} 
       /> 
     </View>
-  );
+  )
 }
 
 export default Products;
