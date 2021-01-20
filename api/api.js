eeb8f92662bc0cf2a714cd0c396f16f2b0a2391e
ilias <iliasdavid@live.fr>
2021-01-20 10:38:42 +0100

    modifs liste Products

--- api/api.js
+++ api/api.js
@@ -1,33 +1,31 @@
-export function getApiConnection() {
+const getApiConnection = async () => {
 		
 	const imageUrl = "http://jouetenbois.mathieu-boucher.fr/api/images/products/";
 	const key = "ws_key=3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE";
     let url = "http://jouetenbois.mathieu-boucher.fr/api/products/?" + key + "&output_format=JSON&display=[id,name,price,id_default_image]";
-	let infos = "[";				
+	var infos = "[";				
 
-    let listOfProducts = fetch(url)
+    fetch(url)
         .then( function(response) {
-			if(response.ok) {
-				console.log("ok");
-			} else
-				console.log("ko");
+			if(response.ok) console.log("ok");
+			else console.log("ko");
 			return response.json();
 		})
 		.then( function(data) {
+
 			data.products.forEach(function (product) {
-				infos += "{ "
+				infos += " { "
 							+ "\"name\": \"" + product.name.shift().value + "\", "
 							+ "\"price\": \"" + product.price + "\", "
 							+ "\"image\": \"" + imageUrl + product.id + "/" + product.id_default_image + "?" + key + "\" "
 						+ "},";		
 			});
-			infos = infos.substring(0, infos.length - 1) + "]";
-			return infos;
+			infos = infos.substring(0, infos.length - 1) + " ]";
+
 		});
 	
-	console.log(infos);
-
-	return JSON.parse(infos);
+	let obj = JSON.parse(infos);
+	return obj;
  
 }
 
--- components/Products.js
+++ components/Products.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { getApiConnection } from "../api/api";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,12 +13,49 @@ const styles = StyleSheet.create({
   },
 });
 
+const getApiConnection = async () => {
+		
+  const imageUrl = "http://jouetenbois.mathieu-boucher.fr/api/images/products/";
+  const key = "ws_key=3MJFI6WYD6GWDGJ8QECM6TZAZ6V65VUE";
+  let url = "http://jouetenbois.mathieu-boucher.fr/api/products/?" + key + "&output_format=JSON&display=[id,name,price,id_default_image]";
+  var infos = "[";				
+
+  await fetch(url)
+  .then( function(response) {
+    if(response.ok) console.log("ok");
+    else console.log("ko");
+    return response.json();
+  })
+  .then( function(data) {
+
+    data.products.forEach(function (product) {
+      infos += " { "
+            + "\"name\": \"" + product.name.shift().value + "\", "
+            + "\"price\": \"" + product.price + "\", "
+            + "\"image\": \"" + imageUrl + product.id + "/" + product.id_default_image + "?" + key + "\" "
+          + "},";		
+    });
+    infos = infos.substring(0, infos.length - 1) + " ]";
+
+  });
+
+  console.log(infos + "\"");
+  var obj = JSON.parse(infos);
+
+  console.log("Product \"" + obj[0].name + "\" :\n"
+			+	"\t- Price : " + obj[0].price + "\n"
+      +	"\t- Image : " + obj[0].image + "\n");
+      
+  return obj; 
+ 
+}
+
 const Products = () => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={getApiConnection()}
-        renderItem={({item}) => <Text>{item.name}</Text>}
+        data={getApiConnection()}  
+        renderItem={{(product)} => <Text>{product.name}</Text>}
       /> 
     </View>
   );
